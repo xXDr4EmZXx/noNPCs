2537445607a1944ddb75ecd1a3796e234ff50cae
xXDr4EmZXx <150351868+xXDr4EmZXx@users.noreply.github.com>
2026-01-02 19:14:42 +0100

    Create fxmanifest.lua for noNPCs script

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'xXDr4EmZXx'
+description 'noNPCs Script - Prevents NPCs from spawning in certain areas.'
+version '1.0.0'
+
+lua54 'yes'
+
+client_scripts {
+    'config.lua',
+    'main.lua'
+}
